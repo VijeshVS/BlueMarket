46a5aabf7a91510992164df8884c98d38293c9b0
Vijesh Shetty <vijeshsshetty@gmail.com>
2023-07-09 15:06:53 +0530

    created forms for register page
    Quit a good job!!! created forms for register page and added some validations

--- bluemarket/__init__.py
+++ bluemarket/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config['SECRET_KEY'] = '46d41ac8259358fac094234d'
 
 db = SQLAlchemy(app)
 app.app_context().push()
--- bluemarket/routes.py
+++ bluemarket/routes.py
@@ -1,6 +1,10 @@
 from bluemarket import app
-from flask import render_template
-from bluemarket.models import Item
+from flask import render_template , redirect , url_for
+from bluemarket.models import Item , User
+from bluemarket.forms import RegisterForm
+from bluemarket import db
+
+# importing the required libaries ----------->
 
 @app.route("/")
 def home_page():
@@ -9,5 +13,28 @@ def home_page():
 
 @app.route("/items")
 def item_page():
-    items = Item.query.all()
+    items = Item.query.all() # retreiving all the objects from the Database from a table named Item and storing  
+                             # it to a variable and passing all the objects to the items.html file
     return render_template("items.html", items=items)
+
+
+@app.route('/register' , methods=['POST','GET'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(
+                              username =form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data
+                              )
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('item_page'))
+
+    #checking if the there is errors or not . Form errors are simple stored in dictionaries and we need to iterate 
+    # through to get the value
+
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(err_msg)        
+    return render_template('register.html' , form=form)
